3466221b1cab5b40f8dd3fcd95541b43128c79f9
Luis Miguel Paz <luismpazv@gmail.com>
2018-11-04 01:09:14 -0500

    SegundoCommit

--- app/src/main/java/develop/app/luismiguelpaz/movistarapp/ActividadPrueba.java
+++ app/src/main/java/develop/app/luismiguelpaz/movistarapp/ActividadPrueba.java
@@ -1,18 +1,48 @@
 package develop.app.luismiguelpaz.movistarapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActividadPrueba extends AppCompatActivity {
 
 
+    Button butEncontrar;
+    TextView textViewResultado;
+
+
+    LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_prueba);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -28,6 +58,34 @@ public class ActividadPrueba extends AppCompatActivity {
         });
 
 
+// Define a listener that responds to location updates
+
+        final LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        textViewResultado=findViewById(R.id.tv_resultado);
+        butEncontrar = findViewById(R.id.but_encontrarUbicacion);
+        butEncontrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ActivityCompat.checkSelfPermission(ActividadPrueba.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ActividadPrueba.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+
+
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000*60, 0, locationListener);
+                Location lastKnown=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                textViewResultado.setText("Lat: "+lastKnown.getLatitude()+", Long:"+lastKnown.getLongitude());
+
+
+            }
+        });
 
     }
 
